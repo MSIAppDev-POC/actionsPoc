1fd06808d71a2a74d6d57d2c71230f2f9ba63821
Sulaiman Gimba <gimbasuleiman@gmail.com>
2022-04-08 14:05:01 -0500

    security and linting added

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,12 +1,14 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get action" do
+  test 'should get action' do
     get home_action_url
     assert_response :success
   end
 
-  test "should get index" do
+  test 'should get index' do
     get home_index_url
     assert_response :success
   end
